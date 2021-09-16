8d460db727b22664ef5ff90f96b4ec95b1129355
lofes <lofes6@gmail.com>
2021-09-16 10:59:25 +0300

    Add files via upload

---
+++ precache-manifest.31043b9504e2d15409e5ad52af214754.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85defddaa28c3efb17d3",
+    "url": "/css/app.4d08c71d.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "716e98bc54bd558853cd75209680687e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d152aad6470e5c8513ad",
+    "url": "/js/about.5a46999e.js"
+  },
+  {
+    "revision": "85defddaa28c3efb17d3",
+    "url": "/js/app.a234aba8.js"
+  },
+  {
+    "revision": "fc6ba606baa5e7d2228d",
+    "url": "/js/chunk-vendors.eff346a5.js"
+  },
+  {
+    "revision": "9ee8ec26cb744a4e2aba834ea8811bf5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
